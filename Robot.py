6671588859526ce6927f872ea8f04661bc56b3e4
sebi <sebastian.rast@stud.schulegl.ch>
2021-06-08 21:54:55 +0200

    added classes

--- Robot.py
+++ Robot.py
@@ -1,38 +1,76 @@
 from simrobot import*
 
-robot = LegoRobot()
+# classes
+class ColorSensor:
 
-gear = Gear()
-lightSensor = LightSensor(SensorPort.S1)
-touchSensor = TouchSensor(SensorPort.S2)
-ultrasonicSensor = UltrasonicSensor(SensorPort.S3)
+    class Color:
+        meassuredBrightnessValue = None
+        upperEnd = None
+        lowerEnd = None
 
-robot.addPart(gear)
-robot.addPart(lightSensor)
-robot.addPart(touchSensor)
-robot.addPart(ultrasonicSensor)
+        def __init__(self, value, band):
+            self.meassuredBrightnessValue = value
+            self.upperEnd = self.meassuredBrightnessValue + band
+            self.lowerEnd = self.meassuredBrightnessValue - band
+
+    whiteValue = None
+    blackValue = None
+    blueValue = None
+
+    def __init__(self, SensorPort, whiteValue, blackValue, blueValue, band): # the variable @band is used to calculate bandwith of the color
+        lightSensor = LightSensor(SensorPort)
+        self.whiteValue = whiteValue
+        self.blackValue = blackValue
+        self.blueValue = blueValue
+
+        self.white = ColorSensor.Color(whiteValue, band)
+        self.black = ColorSensor.Color(blackValue, band)
+        self.blue = ColorSensor.Color(blueValue, band)
+        self.colors = [white, black, blue]
+    
+    def _convertToColor(value):
+        for color in self.colors:
+            for brightness in range(color.lowerEnd, color.upperEnd):
+                if value == brightness:
+                    return color
+            
+        pass
+
+    def getColor():
+        pass
+
+class Robot:
+    def __init__(self):
+        robot = LegoRobot()
+        gear = Gear()
+
+        colorSensor = ColorSensor(SensorPort.S1)
+        touchSensor = TouchSensor(SensorPort.S2)
+        ultrasonicSensor = UltrasonicSensor(SensorPort.S3)
+
+        robot.addPart(gear)
+        robot.addPart(colorSensor.lightSensor)
+        robot.addPart(touchSensor)
+        robot.addPart(ultrasonicSensor)
+
+    def findObject(self):
+        pass
+
+    def goToObject(self):
+        pass
+
+    def getColor(self):
+        pass
+
+    def hitObject(self):
+        pass
+
 
 # variables
 ultraSonicSensorRange = 80
 
-# functions
-def findObject():
+def main():
     pass
 
-def goToObject():
-    pass
-
-def getColor():
-    pass
-
-def hitObject():
-    pass
-
-while not robot.isEscapeHit():
-    gear.right()
-    if ultrasonicSensor.getDistance() < ultraSonicSensorRange:
-        currentMotorTicks = getRightMotorCount()
-        while ultrasonicSensor.getDistance < ultraSonicSensorRange:
-            gear.right()
-        gear.stop()
-        driveTicks = gear.getRightMotorCount()
+if __name__ == "__main__":
+    main()
