293e81a4d2e35e3c5b14a0fd4588900f63b2e799
sebi <sebastian.rast@stud.schulegl.ch>
2021-06-21 21:31:57 +0200

    added method to find out color

--- LegoRobot/main.py
+++ LegoRobot/main.py
@@ -1,28 +1,51 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.ev3devices import (
+    Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+import enum
 
 # classes
+
+
+class ColorCalculator:
+
+    def __init__(self, sensorPort):
+        self.cs = ColorSensor(sensorPort)
+
+    def calculateColor(rgb):
+        color = None
+
+        if rgb[0] != 0:
+            return "WHITE"
+        else:
+            brightness = rgb[0] + rgb[1] + rgb[2]
+
+            if brightness == 0:
+                return "BLACK"
+            else:
+                return "BLUE"
+
 WHEEL_DIAMETER = 55.4
 AXEL_TRACK = 104
-    
+
 class LegoRobot:
     def __init__(self):
         self.robot = EV3Brick()
         self.leftMotor = Motor(Port.A)
         self.rightMotor = Motor(Port.B)
-        self.driveTrain = DriveBase(self.leftMotor, self.rightMotor, wheel_diameter = WHEEL_DIAMETER,  axle_track = AXEL_TRACK)
+        self.driveTrain = DriveBase(
+            self.leftMotor, self.rightMotor, wheel_diameter=WHEEL_DIAMETER,  axle_track=AXEL_TRACK)
 
         self.colorSensor = ColorSensor(Port.S1)
         self.touchSensor = TouchSensor(Port.S2)
         self.ultraSonicSensor = UltrasonicSensor(Port.S3)
 
         # variables
-        self.range = 500  
+        self.range = 500
         self.colorSensorRange = 40  # TODO
 
         self.objectFound = False
@@ -33,7 +56,7 @@ class LegoRobot:
         self.turnRate = 50  # TODO
 
         self.objectCounter = 0
-        
+
         self.deadZone = 10
 
     def findObject(self):
@@ -46,14 +69,13 @@ class LegoRobot:
         while distance > self.range:
             distance = self.ultraSonicSensor.distance()
             wait(self.delay)
-        
+
         self.robot.speaker.beep()
 
         if distance > 1000:
             self.findObject()
 
         self.goToObject()
-
 
     def goToObject(self):
         print("entered goToObject")
@@ -74,13 +96,12 @@ class LegoRobot:
 
             distance = self.ultraSonicSensor.distance()
 
-            print(oldDistance, distance)        
+            print(oldDistance, distance)
             wait(self.delay)
 
         self.driveTrain.stop()
 
         self.getColor()
-
 
     def getColor(self):
         print("entered getColor method")
@@ -91,23 +112,21 @@ class LegoRobot:
         elif color == Color.BLACK:
             self.hitObject()
 
-
     def hitObject(self):
         print("entered hitObjectMethdo")
         self.driveTrain.drive(self.driveSpeed, 0)
 
         while not self.touchSensor.pressed():
             wait(self.delay)
-        
+
         self.driveTrain.stop()
         self.driveTrain.drive(self.driveSpeed, 0)
 
         while self.touchSensor.pressed():
             wait(self.delay)
-        
+
         self.objectCounter += 1
         self.goToNewPlace()
-
 
     def goToNewPlace(self):
         print("entered goToNewPlace")
@@ -115,21 +134,19 @@ class LegoRobot:
         self.driveTrain.turn(90)
         self.driveTrain.straight(200)
         self.findObject()
-    
 
     def run(self):
         self.findObject()
-
 
     def exitProgram(self):
         print(self.objectCounter)
         exit()
 
-
     def checkObjectNumber(self):
-       if self.objectCounter > 5:
-           self.exitProgram()
-       pass
+        if self.objectCounter > 5:
+            self.exitProgram()
+        pass
+
 
 r = LegoRobot()
 r.run()
